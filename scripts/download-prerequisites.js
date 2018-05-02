7b6876be3670ca7fd6682283880264bbef346593
Stéphane MBAPE <smbape@yahoo.fr>
2018-05-02 23:19:22 +0200

    remove unused files fix reinstall on system with old git versions rebuild and test on prepublish

--- scripts/download-prerequisites.js
+++ scripts/download-prerequisites.js
@@ -6,36 +6,31 @@ const mkdirp = require("mkdirp");
 const cwd = sysPath.resolve(__dirname, "../deps");
 const version = "1.9.0";
 
-anyspawn.spawnSeries([
-    mkdirp.bind(null, cwd),
+fs.access(sysPath.join(cwd, "flatbuffers"), err => {
+    let cmd, ccwd;
+    if (err) {
+        cmd = [
+            mkdirp.bind(null, cwd),
 
-    next => {
-        fs.access(sysPath.join(cwd, "flatbuffers"), err => {
-            let cmd, ccwd;
-            if (err) {
-                cmd = [`git clone --depth 1 --branch v${ version } https://github.com/google/flatbuffers.git`];
-                ccwd = cwd;
-            } else {
-                cmd = [
-                    "git clean -xdf",
-                    "git reset --hard HEAD",
-                    `git fetch origin v${ version } --update-shallow`,
-                    `git checkout v${ version }`,
-                ];
-                ccwd = sysPath.join(cwd, "flatbuffers");
-            }
+            `git clone --depth 1 --branch v${ version } https://github.com/google/flatbuffers.git`,
 
-            anyspawn.spawnSeries(cmd, {
-                cwd: ccwd,
+            ["git", ["apply", "-v", sysPath.resolve(__dirname, "../patches/flatbuffers-1.9.x-add_buffer_parsing.patch")], {
+                cwd: sysPath.join(cwd, "flatbuffers"),
                 prompt: true,
                 stdio: "inherit"
-            }, next);
-        });
-    },
+            }]
+        ];
+        ccwd = cwd;
+    } else {
+        cmd = [
+            `git checkout v${ version }`,
+        ];
+        ccwd = sysPath.join(cwd, "flatbuffers");
+    }
 
-    ["git", ["apply", "-v", sysPath.resolve(__dirname, "../patches/flatbuffers-1.9.x-add_buffer_parsing.patch")], {
-        cwd: sysPath.join(cwd, "flatbuffers"),
+    anyspawn.spawnSeries(cmd, {
+        cwd: ccwd,
         prompt: true,
         stdio: "inherit"
-    }]
-]);
+    });
+});
